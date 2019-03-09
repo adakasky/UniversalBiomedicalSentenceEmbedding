489a34161c5490e1f6754024ff42c149394250e5
adakasky <robert910331@sina.com>
2019-03-09 15:45:21 -0500

    change convert_pubmed_to_ospl to multi-processing

--- src/util.py
+++ src/util.py
@@ -6,8 +6,11 @@ import codecs
 import pandas
 import numpy as np
 
+from multiprocessing import Pool
 from gensim.parsing.preprocessing import remove_stopwords, strip_non_alphanum
 from scipy.spatial.distance import cosine
+
+nlp = spacy.load('en_core_web_sm')
 
 
 def load_biosses(d='../data'):
@@ -40,23 +43,30 @@ def compute_pearson_coeff(a, b):
     return np.corrcoef(a, b)[0, 1]
 
 
-def convert_pubmed_to_ospl(file='../data/query_result.csv', out='../data/query_result_ospl.txt'):
+def mp_sent_seg(sent):
+    data = []
+    if type(sent).__name__ == 'str':
+        p = nlp(sent)
+        
+        for s in p.sents:
+            data.append(s.text.lower() + "\n")
+    
+    return data
+
+
+def convert_pubmed_to_ospl(file='../data/query_result.csv', out='../data/query_result_ospl.txt', num_process=8):
+    p = Pool(num_process)
+    
     with codecs.open(file, 'rb', 'utf-8') as r, codecs.open(out, 'wb', 'utf-8') as w:
-        nlp = spacy.load('en_core_web_sm')
-        reader = pandas.read_csv(r, escapechar='\\', chunksize=1)
+        reader = pandas.read_csv(r, escapechar='\\', chunksize=num_process)
         
         for line in reader:
-            title = line['title'].item()
-            abstract = line['abstract'].item()
+            print(line['pmid'])
+            sents = line.values[:, 1:3].flatten()
             
-            if type(title).__name__ == 'str':
-                w.write(title.lower() + "\n")
-                
-            if type(abstract).__name__ == 'str':
-                p = nlp(abstract)
-                
-                for s in p.sents:
-                    w.write(s.text.lower() + "\n")
-            
+            for data in p.imap(mp_sent_seg, sents):
+                w.write("".join(data))
 
-convert_pubmed_to_ospl()
+
+if __name__ == '__main__':
+    convert_pubmed_to_ospl()
